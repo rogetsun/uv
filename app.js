9fb50c72f5b9d78f6a7c2c421b1422a890ceca70
rogetsun <songyw163@163.com>
2015-06-04 13:12:10 +0800

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
+//songyw test
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
